66723c317ca3dd8592906aadd049181edc6770ad
RobsonTrasel <robson.trasel@gmail.com>
2023-12-03 23:11:38 -0300

    feat(command): create open account to an account command

---
+++ account/account.cmd/src/main/java/com/techbank/account/cmd/api/commands/OpenAccountCommand.java
@@ -0,0 +1,13 @@
+package com.techbank.account.cmd.api.commands;
+
+import com.techbank.account.common.dto.AccountType;
+import com.techbank.cqrs.core.commands.BaseCommand;
+import lombok.Data;
+
+@Data
+
+public class OpenAccountCommand extends BaseCommand {
+    private String accountHolder;
+    private AccountType accountType;
+    private double openingBalance;
+}
