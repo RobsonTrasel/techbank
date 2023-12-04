68a1c77dc5e5f84135057e70c16fc2f43b12a7bc
RobsonTrasel <robson.trasel@gmail.com>
2023-12-03 23:36:39 -0300

    chore(dispatcher): add a register and send command dispatcher

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/CommandDispatcher.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/infrastructure/CommandDispatcher.java
@@ -1,4 +1,10 @@
 package com.techbank.cqrs.core.infrastructure;
 
+import com.techbank.cqrs.core.commands.BaseCommand;
+import com.techbank.cqrs.core.commands.CommandHandlerMethod;
+
 public interface CommandDispatcher {
+    <T extends BaseCommand> void registerHandler(Class<T> type, CommandHandlerMethod<T> handler);
+    void send(BaseCommand command);
+
 }
