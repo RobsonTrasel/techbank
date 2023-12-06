56b7daaef3e0b0bd77045c426b93df629375cc61
RobsonTrasel <robson.trasel@gmail.com>
2023-12-06 00:20:26 -0300

    feat(interface): add on methods to type the events handlers

---
+++ account/account.query/src/main/java/com/techbank/account/query/infrastructure/handlers/AccountEventHandler.java
@@ -0,0 +1,58 @@
+package com.techbank.account.query.infrastructure.handlers;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawEvent;
+import com.techbank.account.query.domain.AccountRepository;
+import com.techbank.account.query.domain.BankAccount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountEventHandler implements EventHandler{
+    @Autowired
+    private AccountRepository accountRepository;
+    @Override
+    public void on(AccountOpenedEvent event) {
+        var bankAccount = BankAccount.builder()
+                .Id(event.getId())
+                .accountHolder(event.getAccountHolder())
+                .creationDate(event.getCreatedDate())
+                .accountType(event.getAccountType())
+                .balance(event.getOpeningBalance())
+                .build();
+        accountRepository.save(bankAccount);
+    }
+
+    @Override
+    public void on(FundsDepositedEvent event) {
+        var bankAccount = accountRepository.findById(event.getId());
+        if(bankAccount.isEmpty()) {
+            return;
+        }
+
+        var currentBalance = bankAccount.get().getBalance();
+        var latestBalance = currentBalance + event.getAmount();
+        bankAccount.get().setBalance(latestBalance);
+        accountRepository.save(bankAccount.get());
+    }
+
+    @Override
+    public void on(FundsWithdrawEvent event) {
+        var bankAccount = accountRepository.findById(event.getId());
+        if(bankAccount.isEmpty()) {
+            return;
+        }
+
+        var currentBalance = bankAccount.get().getBalance();
+        var latestBalance = currentBalance - event.getAmount();
+        bankAccount.get().setBalance(latestBalance);
+        accountRepository.save(bankAccount.get());
+    }
+
+    @Override
+    public void on(AccountClosedEvent event) {
+        accountRepository.deleteById(event.getId());
+    }
+}
