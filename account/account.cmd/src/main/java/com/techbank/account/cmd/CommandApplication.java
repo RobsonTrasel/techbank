e0bdedf0269b075d00a6b593fe5179a43a238e2a
RobsonTrasel <robson.trasel@gmail.com>
2023-12-05 23:43:00 -0300

    fix(main): put the right commands on dispatcher

--- account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -1,5 +1,9 @@
 package com.techbank.account.cmd;
 
+import com.techbank.account.cmd.api.commands.*;
+import com.techbank.cqrs.core.infrastructure.CommandDispatcher;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,8 +11,20 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class CommandApplication {
 
+	@Autowired
+	private CommandDispatcher commandDispatcher;
+
+	@Autowired
+	private CommandHandler commandHandler;
 	public static void main(String[] args) {
 		SpringApplication.run(CommandApplication.class, args);
 	}
 
+	@PostConstruct
+	public void registerHandlers() {
+		commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+		commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+	}
 }
