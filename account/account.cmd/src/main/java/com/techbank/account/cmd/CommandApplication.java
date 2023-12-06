340bfd9cbe94a2d10ab1338615c7e2d795ac2a27
RobsonTrasel <robson.trasel@gmail.com>
2023-12-05 23:40:44 -0300

    fix(component): removed unused scna for base packages

--- account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.techbank.cqrs.core.infrastructure", "com.techbank.account.cmd.infrastructure"} )
 public class CommandApplication {
 
 	public static void main(String[] args) {
