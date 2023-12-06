44479d84ead6804c5a46888f309092777943dc66
RobsonTrasel <robson.trasel@gmail.com>
2023-12-05 23:29:13 -0300

    fix(scan): add scan for a specific package

--- account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
+++ account/account.cmd/src/main/java/com/techbank/account/cmd/CommandApplication.java
@@ -2,8 +2,10 @@ package com.techbank.account.cmd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.techbank.cqrs.core.infrastructure", "com.techbank.account.cmd.infrastructure"} )
 public class CommandApplication {
 
 	public static void main(String[] args) {
